a6c3523415b732657ae002e41fd1dbe27c52ac43
brian-guayante <brian-guayante@gmail.com>
2020-12-19 14:53:59 -0800

    end point documentation

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -3,18 +3,21 @@ const router = express.Router();
 
 const Rating = require('../models/Rating');
 
+//GET all ratings//
 router.get('/', (req, res) => {
 	Rating.find().then((allLogs) => {
 		res.json(allLogs);
 	});
 });
 
+//GET rating by ratingId//
 router.get('/:ratingId', (req, res) => {
 	Rating.findById(req.params.ratingId).then((log) => {
 		res.json(log);
 	});
 });
 
+//POST new rating as user with userId//
 router.post('/:userId/new', (req, res) => {
 	Rating.create(req.body)
 		.then({ $set: { user: req.params.userId } })
@@ -23,13 +26,14 @@ router.post('/:userId/new', (req, res) => {
 		});
 });
 
+//DELETE rating by ratingId//
 router.delete('/:ratingId', (req, res) => {
 	Rating.deleteOne({ _id: req.params.ratingId }).then((deleted) => {
 		res.json(deleted);
 	});
 });
 
-//Not Working Yet//
+//UPDATE rating by ratingId//
 router.patch('/:ratingId/edit', (req, res) => {
 	Rating.updateOne({ _id: req.params.ratingId })
 		.then(Rating.find({ _id: req.params.ratingId }))
